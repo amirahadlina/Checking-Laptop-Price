83292ff662647d775ab62634c9a2313cebc82ef8
amirahadlina <107099161+amirahadlina@users.noreply.github.com>
2022-06-18 02:03:29 +0800

    Update code-project.py

--- code-project.py
+++ code-project.py
@@ -1,1 +1,10 @@
+import streamlit as st
+import pandas as pd
+
+from sklearn.ensemble import RandomForestClassifier
+
+st.write("""
+# Simple Prediction App for Laptop Price Range
+This app predicts the **PRICE RANGE** OF LAPTOP!
+""")
 
