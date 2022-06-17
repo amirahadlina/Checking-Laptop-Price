fddc4ca6da8aa3c9536c113378ad11958dbbdaed
amirahadlina <107099161+amirahadlina@users.noreply.github.com>
2022-06-18 02:22:06 +0800

    Update code-project.py

--- code-project.py
+++ code-project.py
@@ -5,5 +5,5 @@ from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
 # Simple Prediction App for Laptop Price Range
-This app lets user choose a few classification methods to predict laptop price ranges based on multiple predictors""")
-
+This app lets user choose a few classification methods to predict laptop price ranges based on multiple predictors.""")
+st.write("""Dataset of this laptop price range can get from here([https://www.kaggle.com/datasets/kuchhbhi/latest-laptop-price-list])
