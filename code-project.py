78cf9f1a7260a4bbfc09f07f2c9a829d4a541875
amirahadlina <107099161+amirahadlina@users.noreply.github.com>
2022-06-18 18:42:11 +0800

    Update code-project.py

--- code-project.py
+++ code-project.py
@@ -4,7 +4,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 
 st.title("""
-# Choose Your Specs and Predict Your Laptop Price!
+Choose Your Specs and Predict Your Laptop Price!
 
 This app let's user predict their laptop price based on multiple specs preferences.""")
 
@@ -12,5 +12,7 @@ my_dataset = 'Cleaned_Laptop_data_newest.csv'
 
 def explore_data(dataset):
   df = pd.read_csv("https://raw.githubusercontent.com/amirahadlina/Checking-Laptop-Price/main/Cleaned_Laptop_data_newest.csv")
+  return df
+
 
 st.sidebar.header('Choose Your Specs Here')
