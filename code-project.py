a93d304fa236eb5f8ba6998f22c08a578eecbfdb
amirahadlina <107099161+amirahadlina@users.noreply.github.com>
2022-06-19 02:36:46 +0800

    Update code-project.py

--- code-project.py
+++ code-project.py
@@ -50,9 +50,9 @@ weight = st.radio("Pick your laptop's weight",["Casual","ThiNlight","Gaming"])
 #elif brand == 'ALIENWARE':
  # brand = 1
   
-new = pd.DataFrame({"choices":df['brand'],'brand_name':['acer','ALIENWARE','APPLE','ASUS','Avita','DELL','HP','iball','Infinix','Lenovo','LG','Mi','MICROSOFT','MSI','Nokia','realme','RedmiBook','SAMSUNG','Smartron','Vaio']}
+abc = pd.DataFrame({"choices":df['brand'],'brand_name':['acer','ALIENWARE','APPLE','ASUS','Avita','DELL','HP','iball','Infinix','Lenovo','LG','Mi','MICROSOFT','MSI','Nokia','realme','RedmiBook','SAMSUNG','Smartron','Vaio']}
                      
-st.table(new)
+st.table(abc)
 
 if st.button("Submit"):
   Xnew = pd.DataFrame([[brand,processor_brand,processor_name,ram_gb,ssd,hdd,weight]],
