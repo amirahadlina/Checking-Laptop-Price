e7b947f763029e758a3b10ce0058d12520ac5e21
amirahadlina <107099161+amirahadlina@users.noreply.github.com>
2022-06-19 00:28:22 +0800

    Update code-project.py

--- code-project.py
+++ code-project.py
@@ -8,17 +8,19 @@ Choose Your Specs and Predict Your Laptop Price!
 
 This app let user predict their laptop price based on multiple specs preferences.""")
 
-my_dataset = 'Cleaned_Laptop_data_newest.csv'
+df = pd.read_csv("https://raw.githubusercontent.com/amirahadlina/Checking-Laptop-Price/main/Cleaned_Laptop_data_newest.csv")
 
-def explore_data(dataset):
-  df = pd.read_csv("https://raw.githubusercontent.com/amirahadlina/Checking-Laptop-Price/main/Cleaned_Laptop_data_newest.csv")
-  return df
+X = pd.df(Cleaned_Laptop_data_newest.data, columns = ['brand','processor','processor_name','ram_GB','ssd','hdd','weight'])
+Y = pd.df(Cleaned_Laptop_data_newest.target, columns = ['latest_price'])
 
-data = explore_data(my_dataset)
+from sklearn.model_selection import train_test_split
+Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size = 0.2, random_state = 999)
 
+laptop = SVC()
+laptop.fit(Xtrain, ytrain)
+ypred = laptop.predict(Xtest)
 
 st.header('Choose Your Specs Here')
-
 
 brand = st.selectbox("Choose your brand", ["Lenovo","Avita","HP","acer","ASUS","DELL","RedmiBook","realme","Infinix","MSI","MICROSOFT","SAMSUNG","Vaio","iball","APPLE","ALIENWARE","Nokia","LG","Smartron","Mi"])
 processor = st.selectbox("Choose your Processor Brand", ["Intel","AMD","MediaTek","M1","Qualcomm"])
@@ -28,8 +30,13 @@ ssd = st.radio("Pick your storage(ssd)",["32GB","128GB","25GB6","512GB","1024GB"
 hdd = st.radio("Pick your storage(hdd)",["512GB","1024GB","2048GB"])
 weight = st.radio("Pick your laptop's weight",["Casual","ThiNlight","Gaming"])
 
-df = pd.read_csv("https://raw.githubusercontent.com/amirahadlina/Checking-Laptop-Price/main/Cleaned_Laptop_data_newest.csv")
-X = pd.df(Cleaned_Laptop_data_newest.data, columns = ['brand','processor','processor_name','ram_GB','ssd','hdd','weight'])
-Y = pd.df(Cleaned_Laptop_data_newest.target, columns = ['latest_price'])
+if st.button("Submit"):
+  Xnew = pd.DataFrame([[brand,processor,processor_name,ram_GB,ssd,hdd,weight]],
+                      column = ["brand","processor","processor_name","ram_GB","ssd","hdd","weight"])
+
+  prediction = laptop.predict(Xnew)[0]
+  
+  st.write(f"Your laptop's price is {predicted}")
+
 
 clicked = st.button("Submit")
