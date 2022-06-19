2fa0e0ab5c4068eaeaaa3d388b0616dd4d92c23e
amirahadlina <107099161+amirahadlina@users.noreply.github.com>
2022-06-19 22:16:07 +0800

    Update code-project.py

--- code-project.py
+++ code-project.py
@@ -21,7 +21,7 @@ df['processor_brand'] = labelencoder.fit_transform(df['processor_brand'])
 df['processor_name'] = labelencoder.fit_transform(df['processor_name'])
 df['weight'] = labelencoder.fit_transform(df['weight'])
 
-st.write(df)
+#st.write(df)
 
 X = df.drop('latest_price', axis = 1)
 y = df['latest_price']
@@ -35,10 +35,28 @@ laptop.fit(Xtrain, ytrain)
 ypred = laptop.predict(Xtest)
 
 st.header('Choose Your Specs Here')
+st.subheader('Please be informed, your Laptop Brand, Processor Brand, Processor Name and Weight is appear in numerical type, e.g: Lenovo = 8')
+st.subheader('Refer to the indicator table given to make your choices')
 
-#brand = st.selectbox("Choose your brand", ["Lenovo","Avita","HP","acer","ASUS","DELL","RedmiBook","realme","Infinix","MSI","MICROSOFT","SAMSUNG","Vaio","iball","APPLE","ALIENWARE","Nokia","LG","Smartron","Mi"])
-#processor_brand = st.selectbox("Choose your Processor Brand", ["Intel","AMD","MediaTek","M1","Qualcomm"])
-#processor_name = st.selectbox("Choose your Processor Name",["Ryzen","Ryzen 3","Ryzen 5","Ryzen 7","Ryzen 9","Quad","Dual core","APU Dual","A6-9225","Athlon Dual","Core i3","Core i5","Core i7","Core i9", "Core m3","Genuine Windows","Plentium Silver","Pentium Quad","GeForce RTX","Hexacore","Everscreenpad","Celeron Dual","MediaTek","SnapDragon","M1"])
+one = pd.DataFrame({"choices":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],'brand_name':['acer','ALIENWARE','APPLE','ASUS','Avita','DELL','HP','iball','Infinix','Lenovo','LG','Mi','MICROSOFT','MSI','Nokia','realme','RedmiBook','SAMSUNG','Smartron','Vaio']})                    
+st.subheader("Brand Indication")
+st.table(one)
+
+two = pd.DataFrame({"choices":[0,1,2,3,4],'processor_brand':['AMD','Intel','M1','MediaTek','Qualcomm']})
+st.subheader("Processor Brand")
+st.table(two)
+
+three = pd.DataFrame({"choices":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],'brand_name':['A6-9225 Processor','APU Dual','Athlon','Celeron Dual','Core i3','Core i5','Core i7','Core m3','Dual Core','Ever Screenpad','GeForce GTX','GeForce RTX','Genuine Windows','Hexa Core','M1','MediaTek','Pentium Quad','Pentium Silver','Quad','Ryzen','Ryzen 3','Ryzen 5','Ryzen 7','Ryzen 9','Snapdragon 7c']})
+st.subheader("Processor Name)
+st.table(three)
+
+four = pd.DataFrame({"choices":[0,1,2],'processor_brand':['Casual','Gaming','ThiNlight']})
+st.subheader("Laptop Weight")
+st.table(four)
+
+#brand = st.selectbox("Your brand", ["Lenovo","Avita","HP","acer","ASUS","DELL","RedmiBook","realme","Infinix","MSI","MICROSOFT","SAMSUNG","Vaio","iball","APPLE","ALIENWARE","Nokia","LG","Smartron","Mi"])
+#processor_brand = st.selectbox("Your Processor Brand", ["Intel","AMD","MediaTek","M1","Qualcomm"])
+#processor_name = st.selectbox("Your Processor Name",["Ryzen","Ryzen 3","Ryzen 5","Ryzen 7","Ryzen 9","Quad","Dual core","APU Dual","A6-9225","Athlon Dual","Core i3","Core i5","Core i7","Core i9", "Core m3","Genuine Windows","Plentium Silver","Pentium Quad","GeForce RTX","Hexacore","Everscreenpad","Celeron Dual","MediaTek","SnapDragon","M1"])
 #ram_gb = st.radio("Choose your RAM",[4,8,16,32])
 #ssd = st.radio("Pick your storage(ssd)",[32,128,256,512,1024])
 #hdd = st.radio("Pick your storage(hdd)",[512,1024,2048])
@@ -47,12 +65,12 @@ st.header('Choose Your Specs Here')
 brand = st.selectbox("Choose your brand", [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19])
 processor_brand = st.selectbox("Choose your Processor Brand", [0,1,2,3,4])
 processor_name = st.selectbox("Choose your Processor Name",[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24])
-#ram_gb = st.radio("Choose your RAM",[4,8,16,32])
-ram_gb = st.radio("Choose your RAM",[0,1,2,3])
-#ssd = st.radio("Pick your storage(ssd)",[0,32,128,256,512,1024])
-#hdd = st.radio("Pick your storage(hdd)",[0,512,1024,2048])
-ssd = st.radio("Pick your storage(ssd)",[0,1,2,3,4,5])
-hdd = st.radio("Pick your storage(hdd)",[0,1,2,3])
+ram_gb = st.radio("Choose your RAM",[4,8,16,32])
+#ram_gb = st.radio("Choose your RAM",[0,1,2,3])
+ssd = st.radio("Pick your storage(ssd)",[0,32,128,256,512,1024])
+hdd = st.radio("Pick your storage(hdd)",[0,512,1024,2048])
+#ssd = st.radio("Pick your storage(ssd)",[0,1,2,3,4,5])
+#hdd = st.radio("Pick your storage(hdd)",[0,1,2,3])
 weight = st.radio("Pick your laptop's weight",[0,1,2])
 
 #if brand == 'acer':
@@ -60,10 +78,22 @@ weight = st.radio("Pick your laptop's weight",[0,1,2])
 #elif brand == 'ALIENWARE':
  # brand = 1
   
-abc = pd.DataFrame({"choices":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],'brand_name':['acer','ALIENWARE','APPLE','ASUS','Avita','DELL','HP','iball','Infinix','Lenovo','LG','Mi','MICROSOFT','MSI','Nokia','realme','RedmiBook','SAMSUNG','Smartron','Vaio']})
-                     
-st.table(abc)
+#one = pd.DataFrame({"choices":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19],'brand_name':['acer','ALIENWARE','APPLE','ASUS','Avita','DELL','HP','iball','Infinix','Lenovo','LG','Mi','MICROSOFT','MSI','Nokia','realme','RedmiBook','SAMSUNG','Smartron','Vaio']})                    
+#st.subheader("Brand Indication")
+#st.table(one)
 
+#two = pd.DataFrame({"choices":[0,1,2,3,4],'processor_brand':['AMD','Intel','M1','MediaTek','Qualcomm']})
+#st.subheader("Processor Brand")
+#st.table(two)
+
+#three = pd.DataFrame({"choices":[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25],'brand_name':['A6-9225 Processor','APU Dual','Athlon','Celeron Dual','Core i3','Core i5','Core i7','Core m3','Dual Core','Ever Screenpad','GeForce GTX','GeForce RTX','Genuine Windows','Hexa Core','M1','MediaTek','Pentium Quad','Pentium Silver','Quad','Ryzen','Ryzen 3','Ryzen 5','Ryzen 7','Ryzen 9','Snapdragon 7c']})
+#st.subheader("Processor Name)
+#st.table(three)
+
+#four = pd.DataFrame({"choices":[0,1,2],'processor_brand':['Casual','Gaming','ThiNlight']})
+#st.subheader("Laptop Weight")
+#st.table(four)
+             
 if st.button("Submit"):
   Xnew = pd.DataFrame([[brand,processor_brand,processor_name,ram_gb,ssd,hdd,weight]],
                       columns = ["brand","processor_brand","processor_name","ram_gb","ssd","hdd","weight"])
